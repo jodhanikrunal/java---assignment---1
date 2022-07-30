7d0ea4ae513ad630bc96cd7835ba9b951da24e19
krunal jodhani <108782510+jodhanikrunal@users.noreply.github.com>
2022-07-30 15:35:24 +0530

    Update pr_3.java

--- pr_3.java
+++ pr_3.java
@@ -4,6 +4,7 @@ public class pr_3 {
 
     public static void lastDigit(int a,int b)
     {
+        //logic for lst digit is same or not
         if(a%10==b%10)
         System.out.println("true");
         
