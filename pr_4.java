621f6baeb7b2a26f35dbcf3e16d48463f354f027
krunal jodhani <108782510+jodhanikrunal@users.noreply.github.com>
2022-07-30 15:37:48 +0530

    Update pr_4.java

--- pr_4.java
+++ pr_4.java
@@ -4,8 +4,10 @@ public class pr_4 {
 
     public static int array123(int[] ar)
     {
+        //initialize counter 0
         int c=0;
 
+        //for loop to check sequence of 1 2 3
         for(int i=0 ;i < ar.length-2 ; i++)
         {
             if(ar[i]==1)
@@ -22,6 +24,7 @@ public class pr_4 {
             else
             continue;
         }
+        //return counter variable
         return c;
     }
 }
